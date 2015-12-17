df7cfbc51f42154a0297bd79602ea40a364a36b5
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-12-17 17:09:25 +0700

    First implementation

--- lib/Text/Table/Org.pm
+++ lib/Text/Table/Org.pm
@@ -9,24 +9,7 @@ use strict;
 use warnings;
 #END IFUNBUILT
 
-package Text::Table::Tiny;
-$Text::Table::Tiny::VERSION = '0.04';
-use parent 'Exporter';
-use List::Util qw();
-
-our @EXPORT_OK = qw/ generate_table /;
-
-# ABSTRACT: makes simple tables from two-dimensional arrays, with limited templating options
-
-
-our $COLUMN_SEPARATOR = '|';
-our $ROW_SEPARATOR = '-';
-our $CORNER_MARKER = '+';
-our $HEADER_ROW_SEPARATOR = '=';
-our $HEADER_CORNER_MARKER = 'O';
-
-sub generate_table {
-
+sub table {
     my %params = @_;
     my $rows = $params{rows} or die "Must provide rows!";
 
@@ -37,11 +20,9 @@ sub generate_table {
     # use that to get the field format and separators
     my $format = _get_format($widths);
     my $row_sep = _get_row_separator($widths);
-    my $head_row_sep = _get_header_row_separator($widths);
 
     # here we go...
     my @table;
-    push @table, $row_sep;
 
     # if the first row's a header:
     my $data_begins = 0;
@@ -49,10 +30,10 @@ sub generate_table {
         my $header_row = $rows->[0];
         $data_begins++;
         push @table, sprintf(
-                         $format,
-                         map { defined($header_row->[$_]) ? $header_row->[$_] : '' } (0..$max_index)
-                     );
-        push @table, $params{separate_rows} ? $head_row_sep : $row_sep;
+            $format,
+            map { defined($header_row->[$_]) ? $header_row->[$_] : '' } (0..$max_index)
+        );
+        push @table, $row_sep;
     }
 
     # then the data
@@ -61,17 +42,25 @@ sub generate_table {
 	    $format,
 	    map { defined($row->[$_]) ? $row->[$_] : '' } (0..$max_index)
 	);
-        push @table, $row_sep if $params{separate_rows};
     }
 
-    # this will have already done the bottom if called explicitly
-    push @table, $row_sep unless $params{separate_rows};
-    return join("\n",grep {$_} @table);
+    return join("", grep {$_} @table);
 }
+
+# FROM_MODULE: List::Util::PP
+# BEGIN_BLOCK: max
+sub max {
+    return undef unless @_;
+    my $res = $_[0];
+    my $i = 0;
+    while (++$i < @_) { $res = $_[$i] if $_[$i] > $res }
+    $res;
+}
+# END_BLOCK: max
 
 sub _get_cols_and_rows ($) {
     my $rows = shift;
-    return ( List::Util::max( map { scalar @$_ } @$rows), scalar @$rows);
+    return ( max( map { scalar @$_ } @$rows), scalar @$rows);
 }
 
 sub _maxwidths {
@@ -81,7 +70,7 @@ sub _maxwidths {
     my $widths = [];
     for my $i (0..$max_index) {
         # go through the $i-th element of each array, find the longest
-        my $max = List::Util::max(map {defined $$_[$i] ? length($$_[$i]) : 0} @$rows);
+        my $max = max(map {defined $$_[$i] ? length($$_[$i]) : 0} @$rows);
         push @$widths, $max;
     }
     return $widths;
@@ -90,29 +79,23 @@ sub _maxwidths {
 # return highest top-index from all rows in case they're different lengths
 sub _max_array_index {
     my $rows = shift;
-    return List::Util::max( map { $#$_ } @$rows );
+    return max( map { $#$_ } @$rows );
 }
 
 sub _get_format {
     my $widths = shift;
-    return "$COLUMN_SEPARATOR ".join(" $COLUMN_SEPARATOR ",map { "%-${_}s" } @$widths)." $COLUMN_SEPARATOR";
+    return "| ".join(" | ",map { "%-${_}s" } @$widths)." |\n";
 }
 
 sub _get_row_separator {
     my $widths = shift;
-    return "$CORNER_MARKER$ROW_SEPARATOR".join("$ROW_SEPARATOR$CORNER_MARKER$ROW_SEPARATOR",map { $ROW_SEPARATOR x $_ } @$widths)."$ROW_SEPARATOR$CORNER_MARKER";
+    return "|-".join("-+-",map { "-" x $_ } @$widths)."-|\n";
 }
-
-sub _get_header_row_separator {
-    my $widths = shift;
-    return "$HEADER_CORNER_MARKER$HEADER_ROW_SEPARATOR".join("$HEADER_ROW_SEPARATOR$HEADER_CORNER_MARKER$HEADER_ROW_SEPARATOR",map { $HEADER_ROW_SEPARATOR x $_ } @$widths)."$HEADER_ROW_SEPARATOR$HEADER_CORNER_MARKER";
-}
-
-# Back-compat: 'table' is an alias for 'generate_table', but isn't exported
-*table = \&generate_table;
 
 1;
 #ABSTRACT: Generate Org tables
+
+=for Pod::Coverage ^(max)$
 
 =head1 SYNOPSIS
 
@@ -136,16 +119,16 @@ two-dimensional array of data as an Org text table.
 
 The example shown in the SYNOPSIS generates the following table:
 
-    | Name  | Rank     | Serial   |
-    |-------+----------+----------|
-    | alice | pvt      | 123456   |
-    | bob   | cpl      | 98765321 |
-    | carol | brig gen | 8745     |
+ | Name  | Rank     | Serial   |
+ |-------+----------+----------|
+ | alice | pvt      | 123456   |
+ | bob   | cpl      | 98765321 |
+ | carol | brig gen | 8745     |
 
 
 =head2 OPTIONS
 
-The C<table> function understands three arguments, which are passed as a hash.
+The C<table> function understands these arguments, which are passed as a hash.
 
 =over
 
@@ -164,8 +147,8 @@ row, and separated from the rest of the table with a ruled line.
 
 =head1 SEE ALSO
 
-This module is basically L<Text::Table::Tiny> 0.03 modified to output Org tables
-instead of its original variant table format.
+This module is currently basically L<Text::Table::Tiny> 0.03 modified to output
+Org tables instead of its original variant table format.
 
 The output of this module is very similar to that of L<Text::MarkdownTable>. In
 fact, Org recognizes its output as a valid Org table (the only difference is
